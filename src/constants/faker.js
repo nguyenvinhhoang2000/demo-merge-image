98c707d607647deefb48fe6de3e466b8dfdc125d
Hoàng Nguyễn Vĩnh <nguyenvinhhoang2000@gmail.com>
2022-11-25 01:21:46 +0700

    create func merge image

--- src/constants/faker.js
+++ src/constants/faker.js
@@ -9,6 +9,66 @@ export const CROWN = [
     label: 'Black Sun',
     value: images.blackSunCrown,
   },
+  {
+    label: 'Daimon Dove',
+    value: images.diamonDoveCrown,
+  },
+  {
+    label: 'Electic Garden',
+    value: images.electicGardenCrown,
+  },
+  {
+    label: 'Erruption',
+    value: images.erruptionCrown,
+  },
+  {
+    label: 'Flame',
+    value: images.flameCrown,
+  },
+  {
+    label: 'Helios',
+    value: images.heliosCrown,
+  },
+  {
+    label: 'Kenshin',
+    value: images.kenshinCrown,
+  },
+  {
+    label: 'Last Samurai',
+    value: images.lastSamuraiCrown,
+  },
+  {
+    label: 'Maiden',
+    value: images.maidenCrown,
+  },
+  {
+    label: 'Myochin',
+    value: images.myochinCrown,
+  },
+  {
+    label: 'Subluminate',
+    value: images.subluminateCrown,
+  },
+  {
+    label: 'Surfer',
+    value: images.surferCrown,
+  },
+  {
+    label: 'Thermakhan',
+    value: images.thermakhanCrown,
+  },
+  {
+    label: 'Thirst',
+    value: images.thirstCrown,
+  },
+  {
+    label: 'Treble',
+    value: images.trebleCrown,
+  },
+  {
+    label: 'Vanian',
+    value: images.vanianCrown,
+  },
 ];
 
 export const DIAL = [
@@ -19,6 +79,66 @@ export const DIAL = [
   {
     label: 'Black Sun',
     value: images.blackSunDial,
+  },
+  {
+    label: 'Daimon Dove',
+    value: images.diamonDoveDial,
+  },
+  {
+    label: 'Electic Garden',
+    value: images.electicGardenDial,
+  },
+  {
+    label: 'Erruption',
+    value: images.erruptionDial,
+  },
+  {
+    label: 'Flame',
+    value: images.flameDial,
+  },
+  {
+    label: 'Helios',
+    value: images.heliosDial,
+  },
+  {
+    label: 'Kenshin',
+    value: images.kenshinDial,
+  },
+  {
+    label: 'Last Samurai',
+    value: images.lastSamuraiDial,
+  },
+  {
+    label: 'Maiden',
+    value: images.maidenDial,
+  },
+  {
+    label: 'Myochin',
+    value: images.myochinDial,
+  },
+  {
+    label: 'Subluminate',
+    value: images.subluminateDial,
+  },
+  {
+    label: 'Surfer',
+    value: images.surferDial,
+  },
+  {
+    label: 'Thermakhan',
+    value: images.thermakhanDial,
+  },
+  {
+    label: 'Thirst',
+    value: images.thirstDial,
+  },
+  {
+    label: 'Treble',
+    value: images.trebleDial,
+  },
+  {
+    label: 'Vanian',
+    value: images.vanianDial,
   },
 ];
 
@@ -31,6 +151,66 @@ export const STRAP = [
     label: 'Black Sun',
     value: images.blackSunStrap,
   },
+  {
+    label: 'Daimon Dove',
+    value: images.diamonDoveStrap,
+  },
+  {
+    label: 'Electic Garden',
+    value: images.electicGardenStrap,
+  },
+  {
+    label: 'Erruption',
+    value: images.erruptionStrap,
+  },
+  {
+    label: 'Flame',
+    value: images.flameStrap,
+  },
+  {
+    label: 'Helios',
+    value: images.heliosStrap,
+  },
+  {
+    label: 'Kenshin',
+    value: images.kenshinStrap,
+  },
+  {
+    label: 'Last Samurai',
+    value: images.lastSamuraiStrap,
+  },
+  {
+    label: 'Maiden',
+    value: images.maidenStrap,
+  },
+  {
+    label: 'Myochin',
+    value: images.myochinStrap,
+  },
+  {
+    label: 'Subluminate',
+    value: images.subluminateStrap,
+  },
+  {
+    label: 'Surfer',
+    value: images.surferStrap,
+  },
+  {
+    label: 'Thermakhan',
+    value: images.thermakhanStrap,
+  },
+  {
+    label: 'Thirst',
+    value: images.thirstStrap,
+  },
+  {
+    label: 'Treble',
+    value: images.trebleStrap,
+  },
+  {
+    label: 'Vanian',
+    value: images.vanianStrap,
+  },
 ];
 
 export const CASE = [
@@ -41,5 +221,65 @@ export const CASE = [
   {
     label: 'Black Sun',
     value: images.blackSunCase,
+  },
+  {
+    label: 'Daimon Dove',
+    value: images.diamonDoveCase,
+  },
+  {
+    label: 'Electic Garden',
+    value: images.electicGardenCase,
+  },
+  {
+    label: 'Erruption',
+    value: images.erruptionCase,
+  },
+  {
+    label: 'Flame',
+    value: images.flameCase,
+  },
+  {
+    label: 'Helios',
+    value: images.heliosCase,
+  },
+  {
+    label: 'Kenshin',
+    value: images.kenshinCase,
+  },
+  {
+    label: 'Last Samurai',
+    value: images.lastSamuraiCase,
+  },
+  {
+    label: 'Maiden',
+    value: images.maidenCase,
+  },
+  {
+    label: 'Myochin',
+    value: images.myochinCase,
+  },
+  {
+    label: 'Subluminate',
+    value: images.subluminateCase,
+  },
+  {
+    label: 'Surfer',
+    value: images.surferCase,
+  },
+  {
+    label: 'Thermakhan',
+    value: images.thermakhanCase,
+  },
+  {
+    label: 'Thirst',
+    value: images.thirstCase,
+  },
+  {
+    label: 'Treble',
+    value: images.trebleCase,
+  },
+  {
+    label: 'Vanian',
+    value: images.vanianCase,
   },
 ];
